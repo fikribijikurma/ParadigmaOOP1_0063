cf8814644549da014609a04423dcfe6fb8c77281
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-05-20 01:14:47 -0700

    menampilkan fungsion pada ibu

--- Anak.h
+++ Anak.h
@@ -6,6 +6,12 @@ public:
 	string nama;
 	vector<anak*> daftar_anak;
 
-	
+	ibu(string pNama) :nama(pNama) {
+		cout << "Ibu" \"" << nama << "\" ada\n";
+	}
+	~ibu() {
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
+ 	}
+
 };
 
